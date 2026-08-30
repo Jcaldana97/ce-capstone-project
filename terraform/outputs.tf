13cf93177aa536bef79571d91c3572bc0ee82b5a
root <root@DESKTOP-SR73RQ3.localdomain>
2026-08-30 21:26:17 +0200

    fix: disable outputs temporary

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,20 +8,20 @@ output "vpc_cidr" {
   value       = aws_vpc.main.cidr_block
 }
 
-output "alb_dns_name" {
-  description = "ALB DNS name — access your application here"
-  value       = aws_lb.main.dns_name
-}
-
-output "alb_url" {
-  description = "Full URL for the application"
-  value       = "http://${aws_lb.main.dns_name}"
-}
-
-output "app_instance_ids" {
-  description = "App instance IDs"
-  value       = aws_instance.app[*].id
-}
+#output "alb_dns_name" {
+#  description = "ALB DNS name — access your application here"
+#  value       = aws_lb.main.dns_name
+#}
+#
+#output "alb_url" {
+#  description = "Full URL for the application"
+#  value       = "http://${aws_lb.main.dns_name}"
+#}
+#
+#output "app_instance_ids" {
+#  description = "App instance IDs"
+#  value       = aws_instance.app[*].id
+#}
 
 output "target_group_arn" {
   description = "Target group ARN"
