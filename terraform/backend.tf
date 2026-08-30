cd9181d46ff46a297ac7c235393725ae65a3b763
root <root@DESKTOP-SR73RQ3.localdomain>
2026-08-30 20:57:35 +0200

    Fix: Correct Terraform formating

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "ce-bootcamp-tfstate-juliocesaraldana"
-    key            = "capstone-project/terraform.tfstate"
-    region         = "us-east-1"
- 
+    bucket = "ce-bootcamp-tfstate-juliocesaraldana"
+    key    = "capstone-project/terraform.tfstate"
+    region = "us-east-1"
+
     use_lockfile = true
     encrypt      = true
   }
