483e97616a7155408932f03ec32e7db6a178acc8
root <root@DESKTOP-SR73RQ3.localdomain>
2026-08-31 11:59:16 +0200

    fix: remove unused input in security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,7 +39,6 @@ module "security_groups" {
   source = "./modules/security-groups"
 
   project_name = var.project_name
-  environment  = var.environment
   vpc_id       = module.vpc_dev.vpc_id
 }
 
