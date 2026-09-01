91a6005a537cc3f4d056baca3d43ef07b2c20ab3
root <root@DESKTOP-SR73RQ3.localdomain>
2026-09-01 09:44:00 +0200

    fix: correct CIDR block

--- terraform/modules/networking/variables.tf
+++ terraform/modules/networking/variables.tf
@@ -17,7 +17,7 @@ variable "availability_zones" {
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = "10.1.0.0/16"
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
